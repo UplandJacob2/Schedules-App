0455e1f3001fddc01f51a7bbaf30f3f5fa8a254b
UplandJacob2 <jacob.feil@stu.evsck12.com>
2024-08-22 07:40:56 -0500

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,21 @@
 //import eslintPlugin from '@typescript-eslint/eslint-plugin'
 //import js from "@eslint/js";
-const eslintPlugin = require("@typescript-eslint/eslint-plugin");
+//const eslintPlugin = require("@typescript-eslint/eslint-plugin");
 const js = require("@eslint/js");
 
 //export default 
 modules.exports = [
+  js.configs.recommended,
   {
     files: ["**.gs", "**.html"],
     ignores: [".github/**"],
-    plugins: { eslintPlugin },
+    plugins: { /*eslintPlugin*/ },
 
-    languageOptions: {
+    /*languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",
       parser: "eslintPlugin/parser",
-    },
+    },*/
 
     rules: {
       semi: "none",
