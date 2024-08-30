db7a001cdb5d67540fd1180c5c48675520a654a3
UplandJacob2 <jacob.feil@stu.evsck12.com>
2024-08-30 08:44:17 -0500

    Update prepare-js-files.js

--- scripts/prepare-js-files.js
+++ scripts/prepare-js-files.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 
-const sourceDir = path.join(__dirname, 'src');
+const sourceDir = __dirname+'src';
 const destinationDir = sourceDir;
+
 fs.readdirSync(sourceDir).forEach(file => {
   if (file.endsWith('.js.html')) {
     const sourcePath = path.join(sourceDir, file);
