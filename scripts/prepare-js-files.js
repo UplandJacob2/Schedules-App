feb828bfae37cdaf5e5ee251542df20649dc9971
UplandJacob2 <jacob.feil@stu.evsck12.com>
2024-08-30 08:40:34 -0500

    Update prepare-js-files.js

--- scripts/prepare-js-files.js
+++ scripts/prepare-js-files.js
@@ -1,9 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
-const sourceDir = '/home/runner/work/Schedules-App/src';
-const destinationDir = '/home/runner/work/Schedules-App/src';
-
+const sourceDir = path.join(__dirname, 'src');
+const destinationDir = sourceDir;
 fs.readdirSync(sourceDir).forEach(file => {
   if (file.endsWith('.js.html')) {
     const sourcePath = path.join(sourceDir, file);
