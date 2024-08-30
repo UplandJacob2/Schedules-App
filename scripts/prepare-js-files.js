55c8c3f259d1380c0f9764e0cbdd9ef353328cce
UplandJacob2 <jacob.feil@stu.evsck12.com>
2024-08-30 07:39:30 -0500

    Create prepare-js-files.js

---
+++ scripts/prepare-js-files.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+const sourceDir = '/src';
+const destinationDir = '/src';
+
+fs.readdirSync(sourceDir).forEach(file => {
+  if (file.endsWith('.js.html')) {
+    const sourcePath = path.join(sourceDir, file);
+    const destPath = path.join(destinationDir, `${file}.js`);
+
+    let content = fs.readFileSync(sourcePath, 'utf8');
+    content = content.replace(/<script>/g, '').replace(/<\/script>/g, '');
+
+    fs.writeFileSync(destPath, content);
+    console.log(`Processed ${file} to ${destPath}`);
+  }
+});
