c0ab70c102e218d4faa913bafe1a661ef2c036b0
UplandJacob2 <169054231+UplandJacob2@users.noreply.github.com>
2024-09-05 13:40:32 -0500

    Update custom-plugin.js

--- eslint/custom-plugin.js
+++ eslint/custom-plugin.js
@@ -1,7 +1,35 @@
 const braceStyleCustom = require('./rules/brace-style-custom')
 
+const globals = require("globals");
+const gas = require("eslint-plugin-googleappsscript")
+
+const globalsServer = require("./globals/gas-server")
+const globalsClient = require("./globals/gas-client")
+const globalsUsedServer = require("./globalsUsed/gas-server")
+const globalsUsedClient = require("./globalsUsed/gas-client")
+
+
 module.exports = {
   rules: {
     "brace-style-custom": braceStyleCustom
+  },
+  globals: {
+    server: {
+      ...globalsServer,
+      ...gas.environment.googleappsscript.globals
+    },
+    client: {
+      ...globalsClient,
+      ...globals.browser,
+      ...globals.jquery
+    }
+  },
+  globalsUsed: {
+    server: [
+      ...globalsUsedServer
+    ],
+    client: [
+      ...globalsUsedClient
+    ]
   }
 }
