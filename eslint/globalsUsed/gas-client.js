d35eaf12b1465e210b30c6850f00c4343c854c17
UplandJacob2 <169054231+UplandJacob2@users.noreply.github.com>
2024-09-05 13:27:17 -0500

    Create gas-client.js

---
+++ eslint/globalsUsed/gas-client.js
@@ -0,0 +1,7 @@
+const globalsUsedVlient = require("../globals/gas-client")
+let clientUsedregex = "(.+_$)"
+const clientGlobalsUsedKeys = Object.keys(globalsUsedClient)
+for (let k = 0; k < clientGlobalsUsedKeys.length; k++) {
+  clientUsedregex += "|(^"+clientGlobalsUsedKeys[k]+"$)"
+}
+module.exports = clientUsedregex
