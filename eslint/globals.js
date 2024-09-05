05d82a5a3e0dbcd0ee338703b3b2df701226260c
UplandJacob2 <169054231+UplandJacob2@users.noreply.github.com>
2024-09-05 12:32:20 -0500

    Create globals.js

---
+++ eslint/globals.js
@@ -0,0 +1,12 @@
+module.exports = {
+  _: "writable",
+  
+  DateUtils: "writable",
+  Base64: "writable",
+  Editor: "writable",
+  DebugLog: "writable",
+
+  l: "writable",
+  w: "writable",
+  e: "writable",
+}
