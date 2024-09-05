362faa9f5218e799711d78f1b70ecd6cde08aa3d
UplandJacob2 <169054231+UplandJacob2@users.noreply.github.com>
2024-09-05 07:30:23 -0500

    Update brace-style-custom.js
    forgot a brace

--- rules/brace-style-custom.js
+++ rules/brace-style-custom.js
@@ -206,7 +206,7 @@ module.exports = {
       },
       IfStatement(node) {
         if (node.consequent.type === "BlockStatement" && node.alternate) {
-          if (astUtils.isTokenOnSameLine(sourceCode.getFirstToken(node.consequent), sourceCode.getLastToken(node.consequent)) {
+          if ( astUtils.isTokenOnSameLine(sourceCode.getFirstToken(node.consequent), sourceCode.getLastToken(node.consequent)) ) {
               validateCurlyBeforeKeyword(sourceCode.getLastToken(node.consequent), "stroustrup");
           } else {
           	// Handle the keyword after the `if` block (before `else`)
