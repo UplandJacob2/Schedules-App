0a88645b8e489e74d064ca9168a12f8d7822fe11
UplandJacob2 <169054231+UplandJacob2@users.noreply.github.com>
2024-09-05 12:33:11 -0500

    Create gas-client.js

---
+++ eslint/globals/gas-client.js
@@ -0,0 +1,41 @@
+const globalsUsed = require("../globals.js")
+const globalsUsedClient = {
+  ...globalsUsed,
+  loadSheet: "writable",
+  saveSettings: "writable",
+  openMenu: "writable",
+  
+  SHEET: "writable",
+  USER_SETTINGS: "writable",
+  
+  getToken: "writable",
+  getEmail: "writable",
+  setCookie: "writable",
+  getCookie: "writable",
+  deleteSignInCookies: "writable",
+  
+  jsonReparse: "writable",
+  confirmPopup: "writable",
+  resizeInput: "writable",
+  toggleDisplay: "writable",
+  error: "writable",
+  
+  signInF: "writable",
+  signUp: "writable",
+  signInSuccess: "writable",
+  signOutBtn: "writable",
+  deleteSch: "writable",
+  editPopClose: "writable",
+  
+  checkSignUpButton: "writable",
+  checkSignInButton: "writable",
+  checkEmailBox: "writable",
+  checkPassBox: "writable",
+  showPassword: "writable",
+  
+  newSchedulesFail: "writable",
+  getSchedulesSuccess: "writable",
+  recoverSchedulesFail: "writable",
+  recoverSchedulesSuccess: "writable",
+  recoverSpecificSchedule: "writable",
+}
