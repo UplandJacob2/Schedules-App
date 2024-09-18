a3facb2c1c1cf5d64fa584afafbf59fb8e8ca603
UplandJacob <123428589+UplandJacob@users.noreply.github.com>
2024-09-17 22:17:03 -0500

    Update prepare-js-files.js

--- scripts/prepare-js-files.js
+++ scripts/prepare-js-files.js
@@ -1,20 +1,20 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const sourceDir = path.join(__dirname, '../src');
+const sourceDir = path.join(__dirname, "../src");
 const destinationDir = sourceDir;
 
 fs.readdirSync(sourceDir).forEach(file => {
-  if(file.endsWith('.js.html')) {
+  if(file.endsWith(".js.html")) {
     const sourcePath = path.join(sourceDir, file);
     const destPath = path.join(destinationDir, `${file}.js`);
 
-    let content = fs.readFileSync(sourcePath, 'utf8');
-    content = content.replace(/<\/?[sS][cC][rR][iI][pP][tT][^>]*>?/g, '')
+    let content = fs.readFileSync(sourcePath, "utf8");
+    content = content.replace(/<\/?[sS][cC][rR][iI][pP][tT][^>]*>?/g, "")
     // ESLint should already do this:
-    content = content.replace(/(?<=\/\/ ?eslint-disable-next-line\n)(.)+(?=\n)/g, '// LINE REMOVED');
-    content = content.replace(/(?<=\n)(.)+(?=\/\/ ?eslint-disable-line\n)/g, '// LINE REMOVED');
-    content = content.replace(/(?<=\/\* ?disable-eslint ?\*\/\n)[\S\s]+(?=\n\/\* ?enable-eslint ?\*\/)/g, '// SECTION REMOVED');
+    content = content.replace(/(?<=\/\/ ?eslint-disable-next-line\n)(.)+(?=\n)/g, "// LINE REMOVED");
+    content = content.replace(/(?<=\n)(.)+(?=\/\/ ?eslint-disable-line\n)/g, "// LINE REMOVED");
+    content = content.replace(/(?<=\/\* ?disable-eslint ?\*\/\n)[\S\s]+(?=\n\/\* ?enable-eslint ?\*\/)/g, "// SECTION REMOVED");
 
 
     fs.writeFileSync(destPath, content);
