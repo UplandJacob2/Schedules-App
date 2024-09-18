6f717df71a52c9f1a42e5df62632b4b27130efe4
UplandJacob2 <169054231+UplandJacob2@users.noreply.github.com>
2024-09-18 11:27:53 -0500

    Update custom-plugin.js

--- eslint/custom-plugin.js
+++ eslint/custom-plugin.js
@@ -46,7 +46,7 @@ Object.assign(plugin.configs, {
       "array-bracket-spacing": [ "warn", "always", {
         "singleValue": false
       } ],
-      "custom/array-element-newline": [ "warn", "consistent", {
+      "custom/array-element-newline": [ "warn", {
         multiline: true,
         multiNotRequired: [ { oneMultiLineItem: true }, 7 ]
       } ],
