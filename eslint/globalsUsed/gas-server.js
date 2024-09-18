44e94209fe8d773308b17f8d9b23485d3d2eb016
UplandJacob <123428589+UplandJacob@users.noreply.github.com>
2024-09-17 22:08:31 -0500

    Update gas-server.js

--- eslint/globalsUsed/gas-server.js
+++ eslint/globalsUsed/gas-server.js
@@ -1,7 +1,7 @@
 const globalsUsedServer = require("../globals/gas-server")
 const serverGlobalsUsedKeys = Object.keys(globalsUsedServer)
 let serverUsedregex = "(.+_$)"
-for (let k = 0; k < serverGlobalsUsedKeys.length; k++) {
+for(let k = 0; k < serverGlobalsUsedKeys.length; k++) {
   serverUsedregex += "|(^"+serverGlobalsUsedKeys[k]+"$)"
 }
 module.exports = serverUsedregex
