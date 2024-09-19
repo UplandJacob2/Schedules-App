bf90b564338910b707653a7b7dbc07779bab2591
UplandJacob2 <169054231+UplandJacob2@users.noreply.github.com>
2024-09-19 07:22:26 -0500

    Update array-element-newline-custom.js

--- eslint/rules/array-element-newline-custom.js
+++ eslint/rules/array-element-newline-custom.js
@@ -149,7 +149,7 @@ module.exports = {
         fix(fixer) {
           const previousToken = sourceCode.getTokenBefore(token, { includeComments: true });
           if(astUtils.isCommentToken(previousToken)) return null;
-          return fixer.removeRange([previousToken.range[1], token.range[0]]);
+          return fixer.removeRange([ previousToken.range[1], token.range[0] ]);
         }
       });
     }
