3095bb6b148301d94cf6b805ec4dd7b46af29107
UplandJacob2 <169054231+UplandJacob2@users.noreply.github.com>
2024-09-27 16:13:51 -0500

    Update sarif_replace_js_html_names.js

--- scripts/sarif_replace_js_html_names.js
+++ scripts/sarif_replace_js_html_names.js
@@ -5,12 +5,12 @@ const sarifFilePath = '/home/runner/work/Schedules-App/Schedules-App/eslint-resu
 const sarifData = JSON.parse(fs.readFileSync(sarifFilePath, 'utf8'));
 
 // Function to replace file names
-const replaceFileNames = (data) => {
-  if (Array.isArray(data)) {
+const replaceFileNames = data => {
+  if(Array.isArray(data)) {
     return data.map(replaceFileNames);
-  } else if (typeof data === 'object' && data !== null) {
-    for (const key in data) {
-      if (key === 'uri' && typeof data[key] === 'string') {
+  } else if(typeof data === 'object' && data !== null) {
+    for(const key in data) {
+      if(key === 'uri' && typeof data[key] === 'string') {
         data[key] = data[key].replace(/\.js\.html\.js$/, '.js.html');
       } else {
         data[key] = replaceFileNames(data[key]);
